a14e69d91603e400138058062f833051e0e927ce
Jason Roberts <osjasonroberts@gmail.com>
2016-10-28 13:06:47 +0800

    Reproduces fk constraint

---
+++ RunMeFirst/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Marten;
+using MyDocs;
+
+
+namespace MyDocs
+{
+    public class A
+    {
+        public int Id { get; set; }
+    }
+
+    public class B
+    {
+        public int Id { get; set; }
+    }
+}
+
+namespace RunMeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string cnString =
+                "host = localhost; database = MartenSpike; password = YOUR PASSWORD HERE; username = postgres";
+            
+            using (var ds = DocumentStore.For(cnString))
+            {
+                ds.Advanced.Clean.CompletelyRemoveAll();
+
+                using (var session = ds.LightweightSession())
+                {
+                    session.Store(new A());
+
+                    session.SaveChanges();
+                }
+            }
+        }
+    }
+}
---
+++ RunMeSecond/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Marten;
+using RunMeSecond.MyDocs;
+
+namespace RunMeSecond
+{
+    namespace MyDocs
+    {
+        public class A
+        {
+            public int Id { get; set; }
+            public int ForeignKeyB { get; set; }
+        }
+
+        public class B
+        {
+            public int Id { get; set; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string cnString =
+               "host = localhost; database = MartenSpike; password = YOUR PASSWORD HERE; username = postgres";
+
+            using (var ds = DocumentStore.For(options =>
+            {
+                options.Connection(cnString);
+
+                options.Schema.For<A>().ForeignKey<B>(x => x.ForeignKeyB);
+            }))
+            {
+               
+                // insert with correct fk
+                using (var session = ds.LightweightSession())
+                {
+                    var b = new B();
+                    session.Store(b);
+
+                    var a = new A {ForeignKeyB = b.Id};
+                    session.Store(a);
+
+                    session.SaveChanges();
+                }
+
+
+
+                // insert with non existent fk
+                using (var session = ds.LightweightSession())
+                {
+                    var nonExistentBId = 3413969;
+                    var a = new A { ForeignKeyB =  nonExistentBId };
+                    session.Store(a);
+
+                    try
+                    {
+                        session.SaveChanges();
+
+                        throw new ApplicationException("SaveChanges should have thrown a foreign key constraint violation exception but did not");
+                    }
+                    catch (Exception)
+                    {                        
+                        throw;
+                    }
+                    
+
+
+
+                }
+            }
+        }
+    }
+}
